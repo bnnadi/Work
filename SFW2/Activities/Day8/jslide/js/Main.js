4e6acd34329db9eaa0c6a8a405c0e7b75908331b
Bisike <bisike.nnadi@gmail.com>
2013-07-30 10:55:30 -0400

    update on Day8 activities

--- SFW2/Activities/Day8/jslide/js/Main.js
+++ SFW2/Activities/Day8/jslide/js/Main.js
@@ -6,7 +6,23 @@
  * To change this template use File | Settings | File Templates.
  */
 
-$(function () {
+$(function(){
 
+      $('#jslider1').jslide({
+          direction: 'vertical',
+          prev: '#slideshow1 .prev',
+          next: '#slideshow1 .next'
+      });
+
+    $('#jslider2').jslide({
+          prev: '#slideshow2 .prev',
+          next: '#slideshow2 .next'
+      });
+
+    $('#jslider3').jslide({
+         // direction: 'vertical',
+          prev: '#slideshow3 .prev',
+          next: '#slideshow3 .next'
+      });
 
 });
