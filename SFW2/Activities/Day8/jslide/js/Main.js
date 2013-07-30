55297180550aab67f28b65eccf846171197e3217
Bisike <bisike.nnadi@gmail.com>
2013-07-30 09:42:45 -0400

    newDay * Lecture Activity

---
+++ SFW2/Activities/Day8/jslide/js/Main.js
@@ -0,0 +1,12 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: bisikennadi
+ * Date: 7/30/13
+ * Time: 9:39 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+$(function () {
+
+
+});
---
+++ SFW2/Activities/Day8/jslide/js/jquery.jslide.js
@@ -0,0 +1,26 @@
+/*
+	jSlide Plugin
+	Version: 2.0
+	Framework: jQuery 1.8+
+	Author: Fullsail Javascript/jQuery Development Team
+	===================++++++=================
+	USAGE
+	--------------
+	<div id="carousel">
+		<ul>
+			<li>Item</li>
+		</ul>
+	</div>
+	
+	$('#carousel').jslide();
+	
+	OPTIONS
+	--------------
+	
+*/
+
+(function($){
+
+
+
+})(jQuery); // end wrapper
